5ab49f4b6c637cd62cf3bd78e78da3695cdcdb49
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-03-30 00:49:21 +0200

    Update and rename product.html to product.php

--- product.php
+++ product.php
@@ -110,19 +110,6 @@
          </header>
          <!-- end header section -->
       </div>
-      <!-- inner page section -->
-      <section class="inner_page_head">
-         <div class="container_fuild">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="full">
-                     <h3>Product Grid</h3>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </section>
-      <!-- end inner page section -->
       <!-- product section -->
       <section class="product_section layout_padding">
          <div class="container">
