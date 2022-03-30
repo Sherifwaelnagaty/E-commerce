3efaa8058848adcfc294043e07ef1e5154cf2a07
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-03-30 21:53:59 +0200

    Add files via upload

---
+++ app/views/inc/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+   <head>
+      <!-- Basic -->
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <!-- Site Metas -->
+      <meta name="keywords" content="" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <link rel="shortcut icon" href="<?php echo URLROOT; ?>images/favicon.png" type="">
+      <title>Famms - Fashion HTML Template</title>
+      <!-- bootstrap core css -->
+      <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>css/bootstrap.css" />
+      <!-- font awesome style -->
+      <link href="<?php echo URLROOT; ?>css/font-awesome.min.css" rel="stylesheet" />
+      <!-- Custom styles for this template -->
+      <link href="<?php echo URLROOT; ?>css/style.css" rel="stylesheet" />
+      <!-- responsive style -->
+      <link href="<?php echo URLROOT; ?>css/responsive.css" rel="stylesheet" />
+   </head>
+
+<body>
+  <?php require APPROOT . '/views/inc/navbar.php'; ?>
+  <div class="container">
+   <!-- jQery -->
+      <script src="<?php echo URLROOT; ?>js/jquery-3.4.1.min.js"></script>
+      <!-- popper js -->
+      <script src="<?php echo URLROOT; ?>js/popper.min.js"></script>
+      <!-- bootstrap js -->
+      <script src="<?php echo URLROOT; ?>js/bootstrap.js"></script>
+      <!-- custom js -->
+      <script src="<?php echo URLROOT; ?>js/custom.js"></script>
