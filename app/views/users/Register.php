4ee9e52a65b4ec2bbec2e036c96731cb6e77edca
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-04-04 13:17:30 +0200

    Update Register.php

--- app/views/users/Register.php
+++ app/views/users/Register.php
@@ -4,18 +4,6 @@ class Register extends view
   public function output()
   {
     require APPROOT . '/views/inc/header.php';
-    $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4"> $title</h1>
-    </div>
-  </div>
-
-  </div>
-  </div>
-  </div>
-EOT;
-    echo $text;
     $this->printForm();
     require APPROOT . '/views/inc/footer.php';
   }
