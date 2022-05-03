4208957fb96297ca75e074d2e11dfa388c9a477c
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-05-03 14:38:05 +0200

    Update Index.php

--- app/views/pages/Index.php
+++ app/views/pages/Index.php
@@ -3,11 +3,6 @@ class Index extends View
 {
   public function output()
   {
-    $title = $this->model->title;
-    $subtitle = $this->model->subtitle;
-    $user_id = $_SESSION['user_id'];
-    $user_name = $_SESSION['user_name'];
-
     require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
     <section class="slider_section ">
