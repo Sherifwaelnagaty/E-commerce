7ae1d57d602be5417bd7413f6d8fecb2b6e07bd1
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-05-12 23:29:04 +0200

    Update ordersmodel.php

--- app/models/ordersmodel.php
+++ app/models/ordersmodel.php
@@ -2,76 +2,25 @@
 require_once 'UserModel.php';
 class ordersmodel extends UserModel
 {
-    public  $title = 'User shopping cart Page';
-    protected $customername;
+    protected $orderID;
     protected $customerID;
-    protected $productname;
-    protected $productID;
-    protected $quantity;
     protected $dateoforder;
-    protected $priceperproduct;
     protected $totalprice;
 
     public function __construct()
     {
         parent::__construct();
-        $this->customername     = "";
         $this->customerID = "";
-
-        $this->productname = "";
-        $this->productID = "";
-        $this->quantity = "";
         $this->dateoforder = "";
-        $this->priceperproduct = "";
         $this->totalprice = "";
     }
-
-    public function getcustomername()
-    {
-        return $this->customername;
-    }
-
-    public function setcustomername($customername)
-    {
-        $this->customername = $customername;
-    }
-
     public function getcustomerID()
     {
         return $this->customerID;
     }
-
     public function setcustomerID($customerID)
     {
         $this->customerID = $customerID;
-    }
-
-    public function getproductname()
-    {
-        return $this->productname;
-    }
-
-    public function setproductname($productname)
-    {
-        $this->productname = $productname;
-    }
-
-    public function getproductID()
-    {
-        return $this->productID;
-    }
-    public function setproductID($productID)
-    {
-        $this->productID = $productID;
-    }
-
-    public function getquantity()
-    {
-        return $this->quantity;
-    }
-    public function setquantity($quantity)
-    {
-        $this->quantity = $quantity;
     }
     public function getdateoforder()
     {
@@ -80,14 +29,6 @@ class ordersmodel extends UserModel
     public function setdateoforder($dateoforder)
     {
         $this->dateoforder = $dateoforder;
-    }
-    public function getpriceperproduct()
-    {
-        return $this->priceperproduct;
-    }
-    public function setpriceperproduct($priceperproduct)
-    {
-        $this->priceperproduct = $priceperproduct;
     }
     public function gettotalprice()
     {
