031d6c1e5606c4bda6683b3c80c18a3303ede2d3
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-05-20 02:13:03 +0200

    Update Account.php

--- app/views/pages/Account.php
+++ app/views/pages/Account.php
@@ -2,48 +2,89 @@
 class Account extends View
 {
   public function output()
-  {
-    $title = $this->model->title;
-    $subtitle = $this->model->subtitle;
-    if(isset($_SESSION['user_id'])){
-    $user_name = $_SESSION['user_name'];
-    $Add=$_SESSION[''];
-    }
-        require APPROOT . '/views/inc/header.php';
-    $text='<div class="container rounded bg-white mt-5 mb-5">
-    <div class="row">
-        <div class="col-md-3 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Edogaru</span><span class="text-black-50"></span><span> </span></div>
-        </div>
-        <div class="col-md-5 border-right"> 
+  { 
+    require APPROOT . '/views/inc/header.php';
+    $this->PrintInfo();
+    require APPROOT . '/views/inc/footer.php';
+  }
+  public function PrintInfo(){
+    $action=URLROOT.'pages/Index';
+    $text=<<<EOT
+            <form action=$action method="post"> 
+            <div class="container rounded bg-white mt-5 mb-5">
+            <div class="row">
+            <div class="col-md-3 border-right">
+            </div>
+            <div class="col-md-5 border-right">
             <div class="p-3 py-5">
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h4 class="text-right">Profile Settings</h4>
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                    <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""><br></div>
-                    <div class="col-md-12"><label class="labels">Address </label><input type="text" class="form-control" placeholder="Enter Your Address " value=""></div>
-                </div>
-                <div class="row mt-3">
-                </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="p-3 py-5"><br><br>
-                <div class="col-md-12"><label class="labels">Email Address</label><input type="text" class="form-control" placeholder="Enter Your Email Address" value=""></div> <br>
-            </div>
-        </div>
-    </div>
-    </div>
-    </div>
-    </div>';
+        EOT;
     echo $text;
-    require APPROOT . '/views/inc/footer.php';
-  }
+    $this->PrintFirstName();
+    $this->printLastName();
+    $this->printMobileNumber();
+    $this->printAddress();
+    $this->printButton();
+    $this->printEmailAddress();
+}
+    public function printFirstName(){
+        $val=$_SESSION['FirstName'];
+        $text=<<<EOT
+                    <div class="col-md-6"><label class="labels">FirstName</label><input type="text" class="form-control" value=$val></div>
+            EOT;
+            echo $text;
+    }
+    public function printLastName(){
+        $val=$_SESSION['LastName'];
+        $text=<<<EOT
+            <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value=$val placeholder="surname"></div>
+                </div>
+            EOT;
+            echo $text;
+    }
+    public function printMobileNumber(){
+        $val=$_SESSION['MobileNumber'];
+        $text=<<<EOT
+            <div class="row mt-3">
+                    <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" value=$val><br></div>
+            EOT;
+            echo $text;
+    }
+    public function printAddress(){
+        $val=$_SESSION['Address'];
+        $text=<<<EOT
+             <div class="col-md-12"><label class="labels">Address</label><textarea type="text" class="form-control" value=$val></textarea></div>
+                </div>
+            EOT;
+            echo $text;
+    }
+    public function printButton(){
+        $text=<<<EOT
+            <div class="row mt-3">
+                </div>
+                <div class="mt-5 text-center"><input type="submit" class="btn btn-primary profile-button" type="button"></div>
+            </div>
+            </div>
+            EOT;
+            echo $text;
+    }
+    public function printEmailAddress(){
+        $val=$_SESSION['Email'];
+        $text=<<<EOT
+            <div class="col-md-4">
+            <div class="p-3 py-5"><br><br>
+                <div class="col-md-12"><label class="labels">Email Address</label><input type="text" class="form-control" placeholder="Enter Your Email Address" value=$val></div> <br>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            EOT;
+            echo $text;
+    }
 }
 ?>
