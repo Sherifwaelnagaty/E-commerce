a761025061b7cff0cc701ac4e8fecc16429434b9
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-05-29 22:42:44 +0200

    Update Products.php

--- app/views/pages/Products.php
+++ app/views/pages/Products.php
@@ -11,62 +11,60 @@ class Products extends View
     $style=URLROOT.'css/Products.css';
     $text = <<<EOT
                 <link rel="stylesheet" href="$style">
-            <section class="product_section layout_padding">
-            <div class="container">
-            <div class="heading_container heading_center">
+                <section class="product_section layout_padding">
+                <div class="container">
+                <div class="heading_container heading_center">
                <h2>
                   Our products
                </h2>
-            </div> 
-            <section class="featured spad">
-            <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="featured__controls">
-                        <ul>
-                            <li class="active" data-filter="*">All</li>
-                            <li data-filter=".Pants">Dresses</li>
-                            <li data-filter=".T-shirts">Blouses</li>
-                            <li data-filter=".Jeans">Cardigans</li>
-                            <li data-filter=".Jackets">Jackets</li>
-                        </ul>
-                    </div>
-                </div>
-                </div>
+                </div> 
+                <section class="product_section layout_padding">
+                <div class="container">
             EOT;
     echo $text;
-    $ProductsNum=$this->model->productcount();
-    while($ProductsNum>0){
     $this->Products();
-    $ProductsNum-=1;
-    }
+    $text=<<<EOT
+            </div>
+            </section>
+            EOT;
+    echo $text;
   }
   public function Products(){
         $Details=URLROOT . 'pages/Products_Detailed';
-        $product_name=$_SESSION['product_name'];
-        $product_type=$_SESSION['product_type'];
-        $product_price=$_SESSION['product_price'];
-        $product_image=$_SESSION['product_image'];
+        $userModel = $this->model;
+        $ProductsNum=$userModel->productcount();
+        $loggedUser=$userModel->product();
+        foreach($loggedUser as $row){
+        $product_name=$row['product_name'];
+        $product_price=$row['product_price'];
+        $product_image=URLROOT .$row['product_image'];
         $text = <<<EOT
-                <div class="row featured__filter">
-                <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
+                    <div class="row">
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Add to cart
+                           </a>
                         </div>
-                        <div class="featured__item__text">
-                            <h6><a href="$Details"></a>$product_name</h6>
-                            <h5>LE $product_price</h5>
-                        </div>
-                    </div>
-                </div>
-                </div>
-                </div>
-                </div>
-              </section>
+                     </div>
+                     <div class="img-box">
+                        <img src="$product_image" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           $product_name
+                        </h5>
+                        <h6>
+                           $product_price
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               </div>
             EOT;
     echo $text;
+    }
   }
 }?>
