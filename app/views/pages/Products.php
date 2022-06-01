ab887f1b6e31f9aeabffe6138dbf765574c8e3df
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-06-01 17:07:06 +0200

    Update Products.php

--- app/views/pages/Products.php
+++ app/views/pages/Products.php
@@ -29,25 +29,26 @@ class Products extends View
             EOT;
     echo $text;
   }
-  public function Products(){
-        $Details=URLROOT . 'pages/Products_Detailed';
+    public function Products(){
         $userModel = $this->model;
-        $ProductsNum=$userModel->productcount();
         $loggedUser=$userModel->product();
         foreach($loggedUser as $row){
+        $action=URLROOT.'pages/Products?id='.$row['productID'];
+        $Details=URLROOT . 'pages/Products_Detailed?id='.$row['productID'];
         $product_name=$row['product_name'];
         $product_price=$row['product_price'];
         $product_image=URLROOT .$row['product_image'];
         $text = <<<EOT
+                    <form action="$action" method="GET">
                     <div class="row">
                     <div class="col-sm-6 col-md-4 col-lg-3">
                     <div class="box">
                      <div class="option_container">
-                        <div class="options">
-                           <a href="" class="option1">
-                           Add to cart
-                           </a>
-                        </div>
+                     <div class="options">
+                           <input type="submit" value="Add to cart">
+                           <form action="$Details" method="GET"><br>
+                           <input type="submit" value="More Info">
+                     </div>
                      </div>
                      <div class="img-box">
                         <img src="$product_image" alt="">
