d5357cdb6db134abf9545f3f68e6858ab09080cf
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2022-06-02 16:02:46 +0200

    Update AccountModel.php

--- app/models/AccountModel.php
+++ app/models/AccountModel.php
@@ -1,110 +1,95 @@
-<?php
-require_once 'UserModel.php';
-class AccountModel extends UserModel
-{
-    protected $firstName;
-    protected $firstNameErr;
-
-    protected $lastName;
-    protected $lastNameErr;
-
-    protected $address;
-    protected $addressErr;
-
-    protected $mobileNum;
-    protected $mobileNumErr;
-    public function __construct()
-    {
-        parent::__construct();
-        $this->firstName = "";
-        $this->firstNameErr = "";
-
-        $this->lastName = "";
-        $this->lastNameErr = "";
-
-        $this->address = "";
-        $this->addressErr = "";
-
-        $this->mobileNum = "";
-        $this->mobileNumErr = "";
-    }
-    public function getFirstName()
+public function getFirst_Name()
     {
         return $this->firstName;
     }
-    public function setFirstName($firstName)
+    public function setFirst_Name($firstName)
     {
         $this->firstName = $firstName;
     }
-    public function getFirstNameErr()
+    public function getFirst_NameErr()
     {
         return $this->firstNameErr;
     }
-    public function setFirstNameErr($firstNameErr)
+    public function setFirst_NameErr($firstNameErr)
     {
         $this->firstNameErr = $firstNameErr;
     }
-    public function getLastName()
+    public function getLast_Name()
     {
         return $this->lastName;
     }
-    public function setLastName($lastName)
+    public function setLast_Name($lastName)
     {
         $this->lastName = $lastName;
     }
-    public function getLastNameErr()
+    public function getLast_NameErr()
     {
         return $this->lastNameErr;
     }
-    public function setLastNameErr($lastNameErr)
+    public function setLast_NameErr($lastNameErr)
     {
         $this->lastNameErr = $lastNameErr;
     }
-    public function getAddress()
+    public function get_Address()
     {
         return $this->address;
     }
-    public function setAddress($address)
+    public function set_Address($address)
     {
         $this->address = $address;
     }
-    public function getAddressErr()
+    public function get_AddressErr()
     {
         return $this->addressErr;
     }
-    public function setAddressErr($addressErr)
+    public function set_AddressErr($addressErr)
     {
         $this->addressErr = $addressErr;
     }
-    public function getMobileNum()
+    public function get_MobileNum()
     {
         return $this->mobileNum;
     }
-    public function setMobileNum($mobileNum)
+    public function set_MobileNum($mobileNum)
     {
         $this->mobileNum = $mobileNum;
     }
-    public function getMobileNumErr()
+    public function get_MobileNumErr()
     {
         return $this->mobileNumErr;
     }
-    public function setMobileNumErr($mobileNumErr)
+    public function set_MobileNumErr($mobileNumErr)
     {
         $this->mobileNumErr = $mobileNumErr;
     }
+    public function get_Email()
+    {
+        return $this->email;
+    }
+    public function set_Email($email)
+    {
+        $this->email = $email;
+    }
+    public function get_EmailErr()
+    {
+        return $this->emailErr;
+    }
+    public function set_EmailErr($emailErr)
+    {
+        $this->emailErr = $emailErr;
+    }
+    public function email_Exist($email)
+    {
+        return $this->findUserByEmail($email) > 0;
+    }
     public function Edit(){
-    $this->dbh->query("UPDATE users SET FirstName=:firstname,LastName=:lastname,Address=:address,Email=:email,MobileNumber=:mobilenum where FirstName=:firstname1 AND LastName= :lastname1 AND Address=:address1 AND Email= :email1 AND MobileNumber=:mobilenum1 AND userID=:userID");
-    $this->dbh->bind(':userID', $this->userid);
+    $this->dbh->query("UPDATE users SET FirstName=:firstname,LastName=:lastname,Address=:address,Email=:email,MobileNumber=:mobilenum where userID=:userID");
+    $this->dbh->bind(':userID', $_SESSION['userID']);
     $this->dbh->bind(':firstname', $this->firstName);
     $this->dbh->bind(':lastname', $this->lastName);
     $this->dbh->bind(':email', $this->email);
     $this->dbh->bind(':address', $this->address);
-    $this->dbh->bind(':mobilenum', $this->mobilenum);
-    $this->dbh->bind(':firstname1', $_SESSION['FirstName']);
-    $this->dbh->bind(':lastname1', $_SESSION['LastName']);
-    $this->dbh->bind(':email1', $_SESSION['Email']);
-    $this->dbh->bind(':address1',$_SESSION['Address']);
-    $this->dbh->bind(':mobilenum1', $_SESSION['MobileNumber']);
+    $this->dbh->bind(':mobilenum', $this->mobileNum);
     return $this->dbh->execute();
     }
 }
